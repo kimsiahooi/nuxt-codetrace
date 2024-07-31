9af002e6b34f088fa117484fe883d428b5e883a0
Ooi Kim Siah <kimsiah@fomatictech.com>
2024-07-31 17:27:21 +0800

    change seo format

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,3 +5,34 @@
     <CustomFooter />
   </div>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  description:
+    'At Codetrace, we understand the evolving landscape of business operations and the increasing need for seamless digital solutions to stay competitive in today’s market. That’s why we offer a comprehensive suite of services aimed at streamlining your business processes and enhancing productivity. From point-of-sale (POS) systems to enterprise resource planning (ERP) solutions, we’ve got you covered every step of the way.',
+  ogTitle: 'Codetrace Business Solution',
+  ogDescription:
+    'At Codetrace, we understand the evolving landscape of business operations and the increasing need for seamless digital solutions to stay competitive in today’s market. That’s why we offer a comprehensive suite of services aimed at streamlining your business processes and enhancing productivity. From point-of-sale (POS) systems to enterprise resource planning (ERP) solutions, we’ve got you covered every step of the way.',
+  ogImage: 'https://codetrace-bs.com.my/wp-content/uploads/2024/04/favicon.png',
+  ogUrl: 'http://localhost:3000',
+  twitterTitle: 'Codetrace Business Solution',
+  twitterDescription:
+    'At Codetrace, we understand the evolving landscape of business operations and the increasing need for seamless digital solutions to stay competitive in today’s market. That’s why we offer a comprehensive suite of services aimed at streamlining your business processes and enhancing productivity. From point-of-sale (POS) systems to enterprise resource planning (ERP) solutions, we’ve got you covered every step of the way.',
+  twitterImage: 'https://codetrace-bs.com.my/wp-content/uploads/2024/04/favicon.png',
+  twitterCard: 'summary',
+})
+
+useHead({
+  htmlAttrs: {
+    lang: 'en',
+  },
+  titleTemplate: '%s - Codetrace Business Solution',
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: 'https://codetrace-bs.com.my/wp-content/uploads/2024/04/favicon.png',
+    },
+  ],
+})
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,23 +19,7 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      htmlAttrs: {
-        lang: 'en',
-      },
-      titleTemplate: '%s - Codetrace Business Solution',
-      meta: [
-        {
-          name: 'description',
-          content:
-            'At Codetrace, we understand the evolving landscape of business operations and the increasing need for seamless digital solutions to stay competitive in today’s market. That’s why we offer a comprehensive suite of services aimed at streamlining your business processes and enhancing productivity. From point-of-sale (POS) systems to enterprise resource planning (ERP) solutions, we’ve got you covered every step of the way.',
-        },
-      ],
       link: [
-        {
-          rel: 'icon',
-          type: 'image/png',
-          href: 'https://codetrace-bs.com.my/wp-content/uploads/2024/04/favicon.png',
-        },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
         {
